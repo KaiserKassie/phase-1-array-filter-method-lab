7e87c88f066d48d62f8760ba9ad27f393c6e8a17
Kassie Becker <kassaundralee@hotmail.com>
2022-02-27 14:30:50 -0600

    commit tree 8d331b58a4459a5d831c7824f6746493afb91d2a

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution here
+const drivers = ["Legolas", "Aragorn", "Gandalf", "Gimli", "Legolas"]
+
+const driversHometowns = [
+    {
+        driver: "Legolas",
+        hometown: "Rivendell",
+    },
+    {
+        driver: "Aragorn",
+        hometown: "Gondor",
+    },
+    {
+        driver: "Gandalf",
+        hometown: "Heaven",
+    },
+    {
+        driver: "Gimli",
+        hometown: "Moria",
+    }
+]
+
+function findMatching(array, string) {
+    const matchingDriver = array.filter(element => element.toUpperCase() === string.toUpperCase())
+    return matchingDriver 
+}
+
+function fuzzyMatch(array, string) {
+    const matchingLetter = array.filter(element => element.startsWith(string))
+    return matchingLetter
+}
+
+function matchName(array, string) {
+    const printHometown = array.filter(element => element.driver === string)
+    return printHometown
+}
+
+
+console.log(matchName(driversHometowns, "Gandalf"))
